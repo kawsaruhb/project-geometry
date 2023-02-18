2adfa1871bffe1a1338e4d3c39631f90d8905a31
kawsaruhb <kawsaruhb@gmail.com>
2023-02-18 19:50:01 +0600

    rectangle area added

--- js/geometry.js
+++ js/geometry.js
@@ -1,18 +1,43 @@
-// Triangle calculation 
-document.getElementById('btn-triangle').addEventListener('click', function(){
-    const triangleFieldElement = document.getElementById('triangle-field-1');
-    const triangleFieldValueString = triangleFieldElement.value;
-    const triangleFieldValue1 = parseFloat(triangleFieldValueString);
+function getFirstInputFieldById(inputId){
+    const FieldElement = document.getElementById(inputId);
+    const FieldValueString = FieldElement.value;
+    const FieldValue1 = parseFloat(FieldValueString);
+    return FieldValue1;
+}
 
-    const triangleFieldElement2 = document.getElementById('triangle-field-2');
-    const triangleFieldValueString2 = triangleFieldElement2.value;
-    const triangleFieldValue2 = parseFloat(triangleFieldValueString2);
+function getSecondInputField2ById(inputId){
+    const FieldElement2 = document.getElementById(inputId);
+    const FieldValueString2 = FieldElement2.value;
+    const FieldValue2 = parseFloat(FieldValueString2);
+    return FieldValue2;
+}
+
+// ---------------------- Triangle calculation --------------------------//
+document.getElementById('btn-triangle').addEventListener('click', function(){
+   
+    const triangleFieldValue1 = getFirstInputFieldById('triangle-field-1');
+    const triangleFieldValue2 = getSecondInputField2ById('triangle-field-2');
 
     const triangleArea = 0.5 * triangleFieldValue1 * triangleFieldValue2;
 
     const areaResult = document.getElementById('result-triangle');
     areaResult.innerText = triangleArea;
-    
-
 })
+
+// --------------------- Rectangle calculation ---------------------------//
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+   
+    const rectangleFieldValue1 = getFirstInputFieldById('rectangle-field-1');
+    const rectangleFieldValue2 = getSecondInputField2ById('rectangle-field-2');
+
+    const rectangleArea = rectangleFieldValue1 * rectangleFieldValue2;
+
+    const areaResult = document.getElementById('result-rectangle');
+    areaResult.innerText = rectangleArea;
+})
+
+
+
+
+
 
